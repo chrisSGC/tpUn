7f7cc72a4600a7eccd7a97e6dc7b09e5368c5386
Christophe Ferru <1941676@cegeprdl.ca>
2020-09-23 13:55:01 -0400

    ajout du menu

---
+++ scripts/js/general.js
@@ -0,0 +1,10 @@
+var fondsPossibles = ['Spacebattle.jpg', 'stargate.jpg', 'stvssw.jps'];
+
+var nomJoueur = "";
+var themeActuel = 0;
+
+function initialiserPartie(){
+    nomJoueur = prompt("Tactical officer on the bridge! Hi sir! Our captain need your name!");
+
+    alert(nomJoueur);
+}
