ff84b0da9318ace2dbdd7fb7348cae61a547288d
Christophe Ferru <1941676@cegeprdl.ca>
2020-09-23 14:08:54 -0400

    affichage du joueur et mise en place des regles

--- scripts/js/general.js
+++ scripts/js/general.js
@@ -6,5 +6,11 @@ var themeActuel = 0;
 function initialiserPartie(){
     nomJoueur = prompt("Tactical officer on the bridge! Hi sir! Our captain need your name!");
 
+    document.getElementById('officerName').innerHTML = nomJoueur;
+
     alert(nomJoueur);
+}
+
+function afficherRegles(){
+    alert("Voici les regles du jeu:\nutilisez Q et D pour un effet surprise\nVous diposez de 60 secondes pour déturire autan d'ennemis que possible");
 }
